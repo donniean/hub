2c6ae1d06f3aebbad5ea96a13724312edea60479
Dongyi An <acmilank22@gmail.com>
2019-06-19 21:27:35 +0800

    update

--- packages/js/chocolatey.js
+++ packages/js/chocolatey.js
@@ -1,6 +1,6 @@
 const config = {
   elementId: 'wrapper',
-  installCommand: 'choco install',
+  installCommand: 'choco install -y',
   uninstallCommand: 'choco uninstall',
   installList: [{ name: 'chocolateygui' }, { name: 'nodejs.install' }],
   uninstallList: []
