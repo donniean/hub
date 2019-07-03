c987ec84ddedebd2a973e91404b2f1d609b96ff1
Dongyi An <acmilank22@gmail.com>
2019-07-03 10:27:53 +0800

    update

--- packages/js/app.js
+++ packages/js/app.js
@@ -1,20 +1,23 @@
-/*global hljs*/
+/* global hljs */
 hljs.initHighlightingOnLoad();
 
 function init({
   elementId,
   installCommand,
   uninstallCommand,
-  installList,
-  uninstallList,
+  installList = [],
+  alternativeList = [],
+  uninstallList = [],
   url
 }) {
   let [
     installTotalCommand,
+    alternativeTotalCommand,
     uninstallTotalCommand,
     installCodeList,
+    alternativeCodeList,
     uninstallCodeList
-  ] = ['', '', [], []];
+  ] = ['', '', '', [], [], []];
 
   if (installList.length > 0) {
     installTotalCommand = installCommand;
@@ -24,6 +27,17 @@ function init({
       const code = `${installCommand} ${url ? link : name}`;
       installCodeList.push(`<li>${getCodeWrapper(code)}</li>`);
       installTotalCommand += ` ${name}`;
+    }
+  }
+
+  if (alternativeList.length > 0) {
+    alternativeTotalCommand = installCommand;
+    for (const item of alternativeList) {
+      const { name } = item;
+      const link = `<a href="${url}${name}" target="_blank">${name}</a>`;
+      const code = `${installCommand} ${url ? link : name}`;
+      alternativeCodeList.push(`<li>${getCodeWrapper(code)}</li>`);
+      alternativeTotalCommand += ` ${name}`;
     }
   }
 
@@ -39,15 +53,25 @@ function init({
   }
 
   installTotalCommand = getCodeWrapper(installTotalCommand);
+  alternativeTotalCommand = getCodeWrapper(alternativeTotalCommand);
   uninstallTotalCommand = getCodeWrapper(uninstallTotalCommand);
 
   const installHtml = `
+    <h1>Install</h1>
     <div>
       <div>${installTotalCommand.trim()}</div>
       <ol>${installCodeList.join('').trim()}</ol>
     </div>
   `;
+  const alternativeHtml = `
+    <h1>Alternative</h1>
+    <div>
+      <div>${alternativeTotalCommand.trim()}</div>
+      <ol>${alternativeCodeList.join('').trim()}</ol>
+    </div>
+  `;
   const uninstallHtml = `
+    <h1>Uninstall</h1>
     <div>
       <div>${uninstallTotalCommand.trim()}</div>
       <ol>${uninstallCodeList.join('').trim()}</ol>
@@ -55,6 +79,7 @@ function init({
   `;
   const html = `
   ${installList.length > 0 ? installHtml : ''}
+  ${alternativeList.length > 0 ? alternativeHtml : ''}
   ${uninstallList.length > 0 ? uninstallHtml : ''}
   `;
   document.getElementById(elementId).innerHTML = html.trim();
--- packages/js/npm.js
+++ packages/js/npm.js
@@ -3,14 +3,16 @@ const config = {
   installCommand: 'npm install -g',
   uninstallCommand: 'npm uninstall -g',
   installList: [
+    { name: '@acmilank22/configs' },
     { name: 'create-react-app' },
     { name: 'htmlhint' },
     { name: 'http-server' },
     { name: 'npm' },
     { name: 'npm-check' },
     { name: 'npm-check-updates' },
-    { name: 'serve' },
+    { name: 'serve' }
   ],
+  alternativeList: [],
   uninstallList: [],
   url: 'https://npms.io/search?q='
 };
