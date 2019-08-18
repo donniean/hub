a35653b7e8dc670619060148ce120a29248bcdbb
Dongyi An <acmilank22@gmail.com>
2019-08-18 22:34:23 +0800

    update

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'color-named': ['never', { ignore: ['inside-function'] }],
     'font-family-name-quotes': 'always-where-recommended',
     'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'selector-type-no-unknown': [true, { ignoreTypes: ['page'] }],
     'value-keyword-case': 'lower',
     'shorthand-property-no-redundant-values': [true, { severity: 'warning' }],
     'property-no-vendor-prefix': [true, { severity: 'warning' }],
