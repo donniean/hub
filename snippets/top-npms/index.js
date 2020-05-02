7a05a93d2798dea48253c909c96b3f2df8315342
Dongyi An <acmilank22@gmail.com>
2020-05-02 19:32:43 +0800

    update

--- snippets/top-npms/index.js
+++ snippets/top-npms/index.js
@@ -24,7 +24,17 @@ const alphabet = [
   'w',
   'x',
   'y',
-  'z'
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'
 ];
 
 const promises = alphabet.map(letter =>
