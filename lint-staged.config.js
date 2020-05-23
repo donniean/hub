5b6ea3f8afa889ac4f804ecd4bf34e18c0e6090a
Dongyi An <acmilank22@gmail.com>
2020-05-23 22:26:58 +0800

    update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  '*.{js,jsx,html,vue,css,scss,json,md}': ['prettier --write', 'git add'],
+  '*.{js,jsx,ts,tsx,html,vue,css,scss,json,md}': [
+    'prettier --write',
+    'git add',
+  ],
   '*.{js,jsx,html,vue}': ['eslint --fix', 'git add'],
-  '*.{css,scss,js,jsx,vue}': ['stylelint --fix', 'git add']
+  '*.{css,scss,js,jsx,vue}': ['stylelint --fix', 'git add'],
 };
