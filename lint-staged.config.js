1db0a164aaf240478882250996251dcae8d0b7f6
Donnie An <acmilank22@gmail.com>
2020-09-18 11:13:13 +0800

    chore: remove unused files

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.{js,ts,jsx,tsx,json,html,vue,css,less,scss,md,yaml}': [
-    'prettier --write',
-    'git add',
-  ],
-  '*.{js,jsx,html,vue}': ['eslint --fix', 'git add'],
-  '*.{css,scss,js,jsx,vue}': ['stylelint --fix', 'git add'],
-};
