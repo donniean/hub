e910c2904d4808dc0641f3bac82fea2867f76b75
Donnie An <acmilank22@gmail.com>
2021-06-09 18:19:32 +0800

    feat: new

---
+++ sources/new/index.js
@@ -0,0 +1,29 @@
+function create(Func, ...args) {
+  // 1. 创建一个空的简单 JavaScript 对象（即{}）
+  const object = {};
+
+  // 2. 将空对象的原型赋值为构造器函数的原型
+  // eslint-disable-next-line no-proto
+  object.__proto__ = Func.prototype;
+
+  // 3. 更改构造器函数内部 this，将其指向新创建的空对象
+  const ret = Func.apply(object, args);
+
+  // 4. 如果该函数没有返回对象，则返回 this
+  return ret instanceof Object ? ret : object;
+}
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.showName = function sayName() {
+  console.log(this.name);
+};
+
+const jack = create(Person, 'jack', 20);
+console.log(jack);
+jack.showName();
+
+module.exports = create;
