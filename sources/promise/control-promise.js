8000bbe32228a6b3e5cc797261a802e12cffb8b8
Donnie An <acmilank22@gmail.com>
2021-06-16 18:25:16 +0800

    feat: promise

---
+++ sources/promise/control-promise.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-plusplus */
+
+function simRequest(url, { duration = 3000, isSuccess = true } = {}) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isSuccess) {
+        resolve({ status: 'success', url, duration });
+      } else {
+        reject(new Error(`${url} failed`));
+      }
+    }, duration);
+  });
+}
+
+function controlPromise(urls, max) {
+  const total = urls.length;
+  const res = [];
+  let sentCount = 0;
+  let completedCount = 0;
+
+  return new Promise((resolve) => {
+    const func = () => {
+      const index = sentCount++;
+
+      if (completedCount >= total) {
+        resolve(res);
+        return;
+      }
+
+      const url = urls[index];
+
+      if (url) {
+        simRequest(url, {
+          duration: (total - index) * 500,
+          isSuccess: index % 2 === 0,
+        })
+          .then((data) => {
+            if (completedCount < total) {
+              completedCount++;
+              res[index] = data;
+              func();
+            }
+          })
+          .catch((error) => {
+            if (completedCount < total) {
+              completedCount++;
+              res[index] = error;
+              func();
+            }
+          });
+      }
+    };
+
+    while (sentCount < max && sentCount < total) {
+      func();
+    }
+  });
+}
+
+controlPromise(['a', 'b', 'c', 'd', 'e', 'f'], 3)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = controlPromise;
