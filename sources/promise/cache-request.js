3bc0314715fc6fb54fca5f1ffca9863c3015972d
Donnie An <acmilank22@gmail.com>
2021-06-18 11:05:32 +0800

    feat: cache request

---
+++ sources/promise/cache-request.js
@@ -0,0 +1,36 @@
+const requestCache = new Map();
+
+function Request(key, { expire = 300 } = {}) {
+  const value = requestCache.get(key);
+
+  const func = () => {
+    const p = fetch(key)
+      .then((res) => res.json())
+      .catch((error) => {
+        requestCache.delete(key);
+        return Promise.reject(error);
+      });
+    requestCache.set(key, { timestamp: new Date().getTime(), promise: p });
+    return p;
+  };
+
+  if (value) {
+    const { timestamp, promise } = value;
+
+    if (new Date().getTime() - timestamp < expire) {
+      return promise;
+    }
+
+    return func();
+  }
+
+  return func();
+}
+
+setInterval(() => {
+  Request('https://api.github.com/search/repositories?q=react').then((res) => {
+    console.log(res);
+  });
+}, 500);
+
+module.exports = Request;
