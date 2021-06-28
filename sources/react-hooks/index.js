1d8a317cfcdd8b36256558d96423bf45e81c646b
Donnie An <acmilank22@gmail.com>
2021-06-28 16:55:47 +0800

    feat: add react hooks

---
+++ sources/react-hooks/index.js
@@ -0,0 +1,16 @@
+const states = [];
+let count = 0;
+
+function useState(defaultState) {
+  const index = count;
+  const setState = (value) => {
+    states[index] = value;
+  };
+
+  count += 1;
+  states[index] = states[index] || defaultState;
+
+  return [states[index], setState];
+}
+
+module.exports = { useState };
