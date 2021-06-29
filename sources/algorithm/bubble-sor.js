fe52a102f3a3aa59f353116887ec525071046fca
Donnie An <acmilank22@gmail.com>
2021-06-29 16:01:47 +0800

    feat: add bubbleSort

---
+++ sources/algorithm/bubble-sor.js
@@ -0,0 +1,18 @@
+function bubbleSort(data) {
+  const res = [...data];
+  const { length } = res;
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < length - 1; i++) {
+    // eslint-disable-next-line no-plusplus
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (res[j] > res[j + 1]) {
+        [res[j], res[j + 1]] = [res[j + 1], res[j]];
+      }
+    }
+  }
+
+  return res;
+}
+
+module.exports = bubbleSort;
