2de2f5bb6c922068a87947e6912c856824023361
Donnie An <acmilank22@gmail.com>
2021-07-08 14:19:28 +0800

    feat: add instanceof

---
+++ sources/instanceof/index.js
@@ -0,0 +1,20 @@
+function customInstanceof(object, constructor) {
+  const constructorPrototype = constructor.prototype;
+
+  while (true) {
+    // eslint-disable-next-line no-proto
+    if (object.__proto__ === null) {
+      return false;
+    }
+
+    // eslint-disable-next-line no-proto
+    if (object.__proto__ === constructorPrototype) {
+      return true;
+    }
+
+    // eslint-disable-next-line no-proto,no-param-reassign
+    object = object.__proto__;
+  }
+}
+
+module.exports = customInstanceof;
