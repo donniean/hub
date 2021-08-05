dca33e63e6425e4a62f18020cc836d876d428aaf
Donnie An <acmilank22@gmail.com>
2021-08-06 00:57:24 +0800

    Update index.js

---
+++ sources/event-emitter/index.js
@@ -0,0 +1,41 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(listener);
+    } else {
+      this.events[eventName] = [listener];
+    }
+  }
+
+  off(eventName, listener) {
+    if (this.events[eventName]) {
+      const index = this.events[eventName].indexOf(listener);
+      if (index !== -1) {
+        this.events[eventName].splice(index, 1);
+      }
+    }
+  }
+
+  once(eventName, listener) {
+    const self = this;
+
+    function func(...args) {
+      listener.call(this, ...args);
+      self.off(eventName, func);
+    }
+
+    self.on(eventName, func);
+  }
+
+  emit(eventName, ...args) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach((listener) =>
+        listener.call(this, ...args)
+      );
+    }
+  }
+}
