0784d889fea2db13d7e1f3dd7725d3b931702057
Donnie An <acmilank22@gmail.com>
2021-08-12 19:48:52 +0800

    feat: add flat

---
+++ sources/flat/index.js
@@ -0,0 +1,20 @@
+function flat(array) {
+  const res = [];
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      res.push(...flat(item));
+    } else {
+      res.push(item);
+    }
+  });
+
+  return res;
+}
+
+const a = [1, [2, 3], 4, 5, [6, [7, [8]]]];
+const r = flat(a);
+
+console.log(r);
+
+module.exports = flat;
