ce3c7aba821c1125b64c9936052422cfe025b5e4
donniean <acmilank22@gmail.com>
2023-03-03 02:35:25 +0800

    feat: add scripts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.{js,css,html,json,md}': 'prettier --write',
   '*.{js,html}': 'eslint --fix',
   '*.css': 'stylelint --fix',
-  '**': 'cspell --no-must-find-files',
+  // '**': 'cspell --no-must-find-files',
 };
