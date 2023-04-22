d2a3edcf140f03b43bf1cd88636fe9ebc0171c95
donniean <acmilank22@gmail.com>
2023-04-23 02:04:37 +0800

    chore: update configs and upgrade dependencies (#21)

---
+++ lint-staged.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  'package.json': 'sort-package-json',
+  '*': [
+    'prettier --write --ignore-unknown',
+    'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
+  ],
+  '*.{js,mjs,cjs}': 'eslint --fix',
+  '*.md': 'markdownlint  --dot --fix',
+};
