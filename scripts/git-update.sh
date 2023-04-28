1d032922076880817e6b27751ff2d1c693a4c1ff
donniean <acmilank22@gmail.com>
2023-04-28 18:14:05 +0800

    chore: add scripts (#28)

---
+++ scripts/git-update.sh
@@ -0,0 +1,11 @@
+cd ~/Workspaces/collections && git checkout main && git fetch && git pull
+
+cd ~/Workspaces/configs && git checkout main && git fetch && git pull
+
+cd ~/Workspaces/proxy && git checkout main && git fetch && git pull
+
+cd ~/Workspaces/react-app && git checkout main && git fetch && git pull
+
+cd ~/Workspaces/styles && git checkout main && git fetch && git pull
+
+cd ~/Workspaces/collections
