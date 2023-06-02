b55adfcdb8bf25bab71f35e956f053dd936deae3
donniean <acmilank22@gmail.com>
2023-06-03 00:30:36 +0800

    feat: update scripts (#47)

--- scripts/npm-global-install.sh
+++ scripts/npm-global-install.sh
@@ -1,2 +1,2 @@
-echo 'npm install -g depcheck http-server npm-check-updates npm-check serve sort-package-json ts-node'
-npm install -g depcheck http-server npm-check-updates npm-check serve sort-package-json ts-node
+echo 'npm install -g @donniean/configs depcheck http-server npm-check-updates npm-check serve sort-package-json ts-node'
+npm install -g @donniean/configs depcheck http-server npm-check-updates npm-check serve sort-package-json ts-node
