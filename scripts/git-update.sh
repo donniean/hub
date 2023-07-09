cbcfe83c0314184da8cad6f09091f28ff7e720ce
donniean <acmilank22@gmail.com>
2023-07-10 00:54:44 +0800

    chore: update scripts (#64)

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -2,6 +2,8 @@ cd ~/Workspaces/collections && git checkout main && git fetch && git pull
 
 cd ~/Workspaces/configs && git checkout main && git fetch && git pull
 
+cd ~/Workspaces/next-app && git checkout main && git fetch && git pull
+
 cd ~/Workspaces/proxy && git checkout main && git fetch && git pull
 
 cd ~/Workspaces/react-app && git checkout main && git fetch && git pull
