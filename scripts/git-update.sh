17a0fcf86c1e76ea59737ee1cf6102c5f1c340bd
donniean <acmilank22@gmail.com>
2023-10-17 08:24:01 +0800

    chore: update scripts

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,13 +1,13 @@
-cd ~/Workspaces/collections && git checkout main && git fetch && git pull
+cd ~/Workspaces/collections && git checkout main && git pull
 
-cd ~/Workspaces/configs && git checkout main && git fetch && git pull
+cd ~/Workspaces/configs && git checkout main && git pull
 
-cd ~/Workspaces/next-app && git checkout main && git fetch && git pull
+cd ~/Workspaces/next-app && git checkout main && git pull
 
-cd ~/Workspaces/proxy && git checkout main && git fetch && git pull
+cd ~/Workspaces/proxy && git checkout main && git pull
 
-cd ~/Workspaces/react-app && git checkout main && git fetch && git pull
+cd ~/Workspaces/react-app && git checkout main && git pull
 
-cd ~/Workspaces/styles && git checkout main && git fetch && git pull
+cd ~/Workspaces/styles && git checkout main && git pull
 
 cd ~/Workspaces/collections
