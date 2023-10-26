2dad14dbce0d9378fe6bfa5e212655e290f7e76e
donniean <acmilank22@gmail.com>
2023-10-26 10:07:49 +0800

    chore: upgrade dependencies (#128)

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,13 +1,13 @@
-cd ~/Workspaces/collections && git checkout main && git pull
+cd ~/Workspaces/collections && git checkout main && git pull --all
 
-cd ~/Workspaces/configs && git checkout main && git pull
+cd ~/Workspaces/configs && git checkout main && git pull --all
 
-cd ~/Workspaces/next-app && git checkout main && git pull
+cd ~/Workspaces/next-app && git checkout main && git pull --all
 
-cd ~/Workspaces/proxy && git checkout main && git pull
+cd ~/Workspaces/proxy && git checkout main && git pull --all
 
-cd ~/Workspaces/react-app && git checkout main && git pull
+cd ~/Workspaces/react-app && git checkout main && git pull --all
 
-cd ~/Workspaces/styles && git checkout main && git pull
+cd ~/Workspaces/styles && git checkout main && git pull --all
 
 cd ~/Workspaces/collections
