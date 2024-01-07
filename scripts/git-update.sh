96a2d1fb53bde589cad154794ebf6b1f6b33f016
donniean <acmilank22@gmail.com>
2024-01-07 23:08:16 +0800

    chore: update configs

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -4,7 +4,7 @@ cd ~/Workspaces/configs && git checkout main && git pull --all
 
 cd ~/Workspaces/next-app && git checkout main && git pull --all
 
-cd ~/Workspaces/proxy && git checkout main && git pull --all
+cd ~/Workspaces/private && git checkout main && git pull --all
 
 cd ~/Workspaces/react-app && git checkout main && git pull --all
 
