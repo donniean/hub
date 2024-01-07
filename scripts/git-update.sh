ef1f266492c9a88c6fcc0af2b334043fa79a1215
donniean <acmilank22@gmail.com>
2024-01-07 23:34:18 +0800

    feat: update sh

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,13 +1,26 @@
-cd ~/Workspaces/collections && git checkout main && git pull --all
+# cd ~/Workspaces/collections && git checkout main && git pull --all
 
-cd ~/Workspaces/configs && git checkout main && git pull --all
+# cd ~/Workspaces/configs && git checkout main && git pull --all
 
-cd ~/Workspaces/next-app && git checkout main && git pull --all
+# cd ~/Workspaces/next-app && git checkout main && git pull --all
 
-cd ~/Workspaces/private && git checkout main && git pull --all
+# cd ~/Workspaces/private && git checkout main && git pull --all
 
-cd ~/Workspaces/react-app && git checkout main && git pull --all
+# cd ~/Workspaces/react-app && git checkout main && git pull --all
 
-cd ~/Workspaces/styles && git checkout main && git pull --all
+# cd ~/Workspaces/styles && git checkout main && git pull --all
+
+# cd ~/Workspaces/collections
+
+function update() {
+  echo "cd ~/Workspaces/$1 && git checkout main && git pull --all"
+  cd ~/Workspaces/$1 && git checkout main && git pull --all
+}
+
+names=(collections configs next-app private react-app styles)
+
+for name in ${names[@]}; do
+  update $name
+done
 
 cd ~/Workspaces/collections
