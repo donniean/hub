874d362ae34acc46afc5cce5157f070a10519e75
donniean <acmilank22@gmail.com>
2024-03-08 01:13:37 +0800

    feat: u
    Signed-off-by: donniean <acmilank22@gmail.com>

--- scripts/oh-my-zsh.plugins.install.sh
+++ scripts/oh-my-zsh.plugins.install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:=~/.oh-my-zsh/custom}"/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}"/plugins/zsh-completions
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
 
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}"/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}"/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/lukechilds/zsh-better-npm-completion "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-better-npm-completion
+git clone https://github.com/lukechilds/zsh-better-npm-completion "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}"/plugins/zsh-better-npm-completion
