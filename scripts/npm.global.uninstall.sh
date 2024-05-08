f3d432894b8c638f270392f00313812915021412
donniean <donniean1@gmail.com>
2024-05-08 09:55:37 +0800

    feat: update npm sh (#356)
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/npm.global.uninstall.sh
+++ scripts/npm.global.uninstall.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+original_dir=$(pwd)
+
+cd "$HOME" || exit
+
 npm uninstall --global \
   @donniean/configs \
   concurrently \
+  corepack \
   depcheck \
   npm-check-updates \
   serve \
   sort-package-json \
   ts-node
+
+# npm uninstall --global npm
+
+cd "$original_dir" || exit
