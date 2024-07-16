b74b6f6750828df659479655d1b3adf372fe4f11
donniean <12584040+donniean@users.noreply.github.com>
2024-07-17 00:01:51 +0800

    chore: update scripts/github.deps.upgrade.sh (#454)
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/github.deps.upgrade.sh
+++ scripts/github.deps.upgrade.sh
@@ -4,14 +4,29 @@ original_dir=$(pwd)
 
 pr() {
   cd "$HOME"/repos/"$1" || exit
+
+  # checkout and pull
+  git checkout main
+  git branch -D chore
+  git pull --all --prune
+
+  # upgrade dependencies
   git checkout -b chore
   ncu --upgrade
   rm -rf node_modules
   rm package-lock.json
   npm install
+
+  # commit and push
   git commit --all --signoff --gpg-sign --message 'chore: bump dependencies'
   git push --set-upstream origin chore
-  open "https://github.com/donniean/$1/compare/main...chore"
+
+  # create and merge Pull Request
+  gh pr create --fill
+  # pr_output=$(gh pr create --fill)
+  # pr_url=$(echo "$pr_output" | grep -o 'https://github\.com/[^ ]*')
+  gh pr merge --auto --squash
+  gh pr view --web
 }
 
 names=(
