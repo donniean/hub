785465454084f9664240b914aac22f26b073cf68
donniean <donniean1@gmail.com>
2025-03-03 14:54:40 +0800

    chore: enable eslint auto-fix for JavaScript and TypeScript files
    Signed-off-by: donniean <donniean1@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -6,7 +6,7 @@ export default {
     'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
   ],
   // '*.ts': 'bash -c tsc --noEmit',
-  // '*.{js,mjs,cjs,ts}': 'eslint --fix',
+  '*.{js,mjs,cjs,ts}': 'eslint --fix',
   '*.css': 'stylelint --fix',
   '*.html': 'htmlhint',
   '*.md': 'markdownlint --dot --fix',
