7fc538f77f8167d2e4f1ec9b965f06cc0dd97972
donniean <donniean1@gmail.com>
2025-05-15 14:36:55 +0800

    feat: add initial cspell configuration with custom words and ignore paths
    Signed-off-by: donniean <donniean1@gmail.com>

---
+++ cspell.config.mjs
@@ -0,0 +1,74 @@
+import { defineConfig } from 'cspell';
+
+export default defineConfig({
+  version: '0.2',
+  language: 'en',
+  enableGlobDot: true,
+  words: [
+    // apps
+    'webstorm',
+    'wechat',
+    // brands
+    'Vercel',
+    // Docker
+    'Buildx',
+    // files
+    'autocorrectignore',
+    'autocorrectrc',
+    'browserslistrc',
+    'commitlintrc',
+    'configsrc',
+    'htmlhintrc',
+    'huskyrc',
+    'lintstagedrc',
+    'markdownlintignore',
+    'stylelintignore',
+    'stylelintrc',
+    // Git
+    'signoff',
+    // nginx
+    'proxied',
+    // npm
+    'autocorrect',
+    'clsx',
+    'commitlint',
+    'corepack',
+    'cssnano',
+    'deepmerge',
+    'donniean',
+    'htmlhint',
+    'immer',
+    'markdownlint',
+    'npmjs',
+    'numbro',
+    'rimraf',
+    'sonarjs',
+    'stylelint',
+    'svgr',
+    'tailwindcss',
+    'tanstack',
+    'tsup',
+    'vitest',
+    'zustand',
+    // TypeScript
+    'classname',
+  ],
+  ignorePaths: [
+    '.git/',
+    '.idea/',
+    '.vscode/',
+    '**/*.min.*',
+    '**/*.svg',
+    '**/package.json',
+    '**/package-lock.json',
+    '**/yarn.lock',
+    '**/pnpm-lock.yaml',
+    '**/Dockerfile',
+    '**/CHANGELOG.md',
+    '**/.htmlhintrc',
+    '.*ignore',
+    '.gitattributes',
+    // custom
+    'macOS/',
+  ],
+});
