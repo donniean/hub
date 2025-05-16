6420fe6beee29ac0e68011c71879ce055b668ca0
donniean <12584040+donniean@users.noreply.github.com>
2025-05-17 03:13:20 +0800

    chore: update configs (#1023)
    * chore: remove pre-commit script from package.json
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: ypdate configs
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: add 'macOS' to ignorePaths in cspell configuration
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: udate configs
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * fix: correct macOS path in cspell configuration
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    ---------
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,12 +1,15 @@
+/**
+ * @type {import('lint-staged').Configuration}
+ */
 export default {
   'package.json': 'sort-package-json',
   '*': [
     'prettier --write --ignore-unknown',
     'autocorrect --fix',
-    'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
+    'cspell lint --no-progress --no-must-find-files --dot --gitignore',
   ],
-  // '*.ts': 'bash -c tsc --noEmit',
-  // '*.{js,mjs,cjs,ts}': 'eslint --fix',
+  // '*.ts': ['bash -c tsc --noEmit', 'vitest related --run'],
+  // '*.{js,mjs,cjs,ts}': 'eslint --fix --max-warnings 0',
   '*.css': 'stylelint --fix',
   '*.html': 'htmlhint',
   '*.md': 'markdownlint --dot --fix',
