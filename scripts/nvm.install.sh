61a27014595e7c220cd5df75349b7cf5efda5b1c
donniean <donniean1@gmail.com>
2025-05-20 15:19:30 +0800

    chore: enhance nvm.install.sh script with error handling and comments
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/nvm.install.sh
+++ scripts/nvm.install.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+set -e
+
+# https://github.com/nvm-sh/nvm
+
+TAG=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest |
+  grep -o '"tag_name": "[^"]*' | cut -d'"' -f4)
+
+# https://github.com/nvm-sh/nvm?tab=readme-ov-file#install--update-script
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/"$TAG"/install.sh |
+  bash
