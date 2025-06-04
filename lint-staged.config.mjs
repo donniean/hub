b56f8fc1ca34559b6e079646842c870a323ae26d
donniean <12584040+donniean@users.noreply.github.com>
2025-06-04 17:31:54 +0800

    chore: update eslint command in lint-staged configuration (#1099)
    * chore: update eslint command in lint-staged configuration
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: remove unnecessary eslint-disable comment from eslint configuration
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: remove unused dependencies from package.json
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    ---------
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -9,7 +9,7 @@ export default {
     'cspell lint --no-progress --no-must-find-files --dot --gitignore',
   ],
   // '*.ts': ['bash -c tsc --noEmit', 'vitest related --run'],
-  // '*.{js,mjs,cjs,ts}': 'eslint --fix --max-warnings 0',
+  // '*.{js,mjs,cjs,ts}': 'eslint --fix', // --max-warnings 0
   '*.css': 'stylelint --fix',
   '*.html': 'htmlhint',
   '*.md': 'markdownlint --dot --fix',
