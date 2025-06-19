67c60338e7874f8ed6c455d000785da196335166
donniean <12584040+donniean@users.noreply.github.com>
2025-06-19 13:36:34 +0800

    feat: migrate styles and scripts to playground directory with base styles wrapped in @layer (#1155)
    * chore(styles): wrap base styles in @layer directive
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore(styles): migrate styles and scripts to playground directory
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore(favicon): add favicon.svg for the Playground application
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore(favicon): remove favicon.svg and comment out icon link in index.html
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    ---------
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- apps/playground/index.js
+++ apps/playground/index.js
@@ -1,5 +1,5 @@
 /* eslint import-x/no-unresolved: [2, { ignore: ['^https:\/\/'] }] */
 
-import _ from 'https://esm.run/lodash';
+import _ from 'https://cdn.jsdelivr.net/npm/lodash/+esm';
 
 export const version = _.VERSION;
