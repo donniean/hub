6ca35162f824263e47166865389afcb8d56d8572
donniean <donniean1@gmail.com>
2025-12-22 13:44:34 +0800

    chore(vscode): drop personal vscode configs -【chore】remove vscode keybindings and settings resources to avoid tracking local prefs -【chore】delete helper scripts and package hooks that synced settings now that configs are gone
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/vscode/paths.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dir_path=$(dirname "$0")
-
-source "${dir_path}"/../paths.sh
-
-export src_path="${root_path}/resources/vscode/settings.json"
-
-export dest_path="$HOME/Library/Application Support/Code/User/settings.json"
--- scripts/vscode/src-to-dest.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-dir_path=$(dirname "$0")
-
-source "${dir_path}"/paths.sh
-
-cp "$src_path" "$dest_path"
