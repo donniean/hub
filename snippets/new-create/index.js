c610461c9254c99b687051c2b652770be463cf71
donniean <donniean1@gmail.com>
2026-03-25 14:52:04 +0800

    refactor(algorithm): fix path building in findPath - append current node id to path using array spread
    refactor(new-create): simplify customNew object prototype
    - initialize object with __proto__ via object literal syntax
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- snippets/new-create/index.js
+++ snippets/new-create/index.js
@@ -1,11 +1,8 @@
 function customNew(Func, ...args) {
   // 1. 创建一个空的简单 JavaScript 对象（即{}）
-
-  const object = {};
-
   // 2. 将空对象的原型赋值为构造函数的原型
 
-  object.__proto__ = Func.prototype;
+  const object = { __proto__: Func.prototype };
 
   // 3. 更改构造器函数内部 this，将其指向新创建的空对象
   const ret = Func.apply(object, args);
