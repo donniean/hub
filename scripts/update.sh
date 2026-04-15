027fca3e13421c69f87485cd40496a288fe8a010
donniean <donniean1@gmail.com>
2026-04-15 15:35:26 +0800

    chore: remove nvm install step from update script
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,8 +9,6 @@ source "${basePath}"/brew.upgrade.sh
 
 source "${basePath}"/mas.upgrade.sh
 
-source "${basePath}"/nvm.install.sh
-
 source "${basePath}"/npm.global.update.sh
 
 source "${basePath}"/github.repos.update.sh
