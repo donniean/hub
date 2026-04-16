be892e71931b7734126da10da13e23c14f282487
donniean <donniean1@gmail.com>
2026-04-16 14:38:05 +0800

    chore(scripts): add binary location checks for tools - add `type -a` before running tool version commands - improves diagnostics by showing all matching tool paths
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,37 +7,48 @@ export PS4='+CMD [${BASH_SOURCE##*/}:${LINENO}] '
 date '+%Y-%m-%d %H:%M:%S %z'
 
 command -V ast-grep
+type -a ast-grep
 ast-grep --version
 
 command -V bash
+type -a bash
 bash --version
 
 command -V curl
+type -a curl
 curl --version
 
 command -V fd
+type -a fd
 fd --version
 
 command -V gh
+type -a gh
 gh --version
 gh auth status
 
 command -V git
+type -a git
 git --version
 
 command -V jq
+type -a jq
 jq --version
 
 command -V rg
+type -a rg
 rg --version
 
 command -V uv
+type -a uv
 uv --version
 
 command -V yq
+type -a yq
 yq --version
 
 command -V zsh
+type -a zsh
 zsh --version
 
 npx --yes --package=@playwright/cli playwright-cli --version
