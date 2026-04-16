b15a2f2590d86d04db3cd31f4832ea1ad74eac9f
donniean <donniean1@gmail.com>
2026-04-16 14:31:15 +0800

    chore(scripts): disable set -e in bash scripts
    - remove `-e` from `set` flags to avoid exiting on command errors
    - keep `-u` and `pipefail` for safer variable handling and pipelines
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,54 +1,43 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 export PS4='+CMD [${BASH_SOURCE##*/}:${LINENO}] '
 
 date '+%Y-%m-%d %H:%M:%S %z'
 
 command -V ast-grep
-where ast-grep
 ast-grep --version
 
 command -V bash
-where bash
 bash --version
 
 command -V curl
-where curl
 curl --version
 
 command -V fd
-where fd
 fd --version
 
 command -V gh
-where gh
 gh --version
 gh auth status
 
 command -V git
-where git
 git --version
 
 command -V jq
-where jq
 jq --version
 
 command -V rg
-where rg
 rg --version
 
 command -V uv
-where uv
 uv --version
 
 command -V yq
-where yq
 yq --version
 
 command -V zsh
-where zsh
 zsh --version
 
 npx --yes --package=@playwright/cli playwright-cli --version
