1ddd5d3ef1a21f9dfcb938d124b93243a85a1da3
donniean <donniean1@gmail.com>
2026-04-17 01:27:06 +0800

    chore(scripts): ensure mas repo listing before upgrade
    - enable -e to fail fast on errors
    - run `mas list` prior to outdated/upgrade to validate state
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/mas.upgrade.sh
+++ scripts/mas.upgrade.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -euo pipefail
 
 export PS4='+CMD [${BASH_SOURCE##*/}:${LINENO}] '
 
 set -x
 
 date '+%Y-%m-%dT%H:%M:%S%z'
+
+mas list
 
 mas outdated
 
