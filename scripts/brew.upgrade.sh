d200829d85d5ebcfb91d8f70b5fcaf28ad0e37b5
donniean <donniean1@gmail.com>
2026-04-18 00:09:07 +0800

    chore(scripts): split brew upgrade for formula/cask - run dry-run upgrades separately for formula and cask - run actual upgrades separately for formula and cask
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/brew.upgrade.sh
+++ scripts/brew.upgrade.sh
@@ -13,8 +13,11 @@ brew update
 brew outdated --formula --verbose
 brew outdated --cask --greedy --verbose
 
-brew upgrade --display-times --greedy --dry-run
-brew upgrade --display-times --greedy
+brew upgrade --formula --display-times --dry-run
+brew upgrade --cask --greedy --display-times --dry-run
+
+brew upgrade --formula --display-times
+brew upgrade --cask --greedy --display-times
 
 brew autoremove
 brew cleanup
