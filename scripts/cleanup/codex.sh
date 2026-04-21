62aec00204a08b62d0fd1647bf916c01d31f9ae9
donniean <donniean1@gmail.com>
2026-04-21 16:26:44 +0800

    chore(cleanup): list codex archived sessions before delete
    - add eza tree output for ${TARGET_DIR} to aid cleanup visibility
    - remove archived_sessions by targeting a single TARGET_DIR var
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/cleanup/codex.sh
+++ scripts/cleanup/codex.sh
@@ -6,4 +6,7 @@ export PS4=$'\n+CMD [\D{%Y-%m-%dT%H:%M:%S%z}] [${BASH_SOURCE##*/}:${LINENO}] '
 
 set -x
 
-rm -rf "${HOME}/.codex/archived_sessions/"
+TARGET_DIR="${HOME}/.codex/archived_sessions/"
+
+eza --tree --all --group-directories-first "${TARGET_DIR}"
+rm -rf "${TARGET_DIR}"
