345b420189cd04ab3be3b827061760e32dad8f19
donniean <12584040+donniean@users.noreply.github.com>
2026-06-17 22:37:00 +0800

    chore(cspell): normalize dictionary casing (#1855)
    Signed-off-by: donniean <donniean1@gmail.com>

--- cspell.config.mjs
+++ cspell.config.mjs
@@ -9,9 +9,9 @@ export default defineConfig({
     'webstorm',
     'wechat',
     // brands
-    'Vercel',
+    'vercel',
     // Docker
-    'Buildx',
+    'buildx',
     // files
     '.autocorrectignore',
     '.autocorrectrc',
@@ -57,7 +57,7 @@ export default defineConfig({
     'nodenext',
     'classname',
     // Vite
-    'VITE',
+    'vite',
     // custom
     'donniean',
   ],
