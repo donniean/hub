7bf0cda403329aaff88e35910f9b7d799a46db54
donniean <donniean1@gmail.com>
2026-06-23 00:39:27 +0800

    chore: revert lint-staged script wrapping
    Signed-off-by: donniean <donniean1@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,13 +3,13 @@
  */
 export default {
   '*': [
-    'pnpm run format:oxfmt -- --no-error-on-unmatched-pattern',
-    'pnpm run lint:autocorrect:fix --',
-    () => 'pnpm run lint:spellcheck',
+    'oxfmt --no-error-on-unmatched-pattern',
+    'autocorrect --fix',
+    'cspell lint --no-progress --no-must-find-files --dot --gitignore',
   ],
-  // '*.{ts,tsx,mts,cts}': [() => 'pnpm run typecheck', 'vitest related --run'],
-  // '*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}': 'pnpm run lint:oxlint:fix --',
-  '*.css': () => 'pnpm run lint:styles:fix',
-  '*.html': () => 'pnpm run lint:html',
-  '*.md': () => 'pnpm run lint:markdown:fix',
+  // '*.{ts,tsx,mts,cts}': [() => 'tsc --noEmit', 'vitest related --run'],
+  // '*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}': 'oxlint --fix',
+  '*.css': 'stylelint --fix',
+  '*.html': 'html-validate',
+  '*.md': 'markdownlint --dot --fix',
 };
