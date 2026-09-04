d926e863b2d1d78e864622c1236bf4794ec7d4e7
donniean <donniean1@gmail.com>
2026-09-04 14:20:58 +0800

    chore(inspect): update installed apps inspection script
    - remove obsolete homebrew diagnostic and list commands
    - simplify mac app store list command output
    - add conditional check and inspection for user applications directory
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- scripts/inspect/apps.sh
+++ scripts/inspect/apps.sh
@@ -6,20 +6,12 @@ export PS4=$'\n+CMD [\D{%Y-%m-%dT%H:%M:%S%z}] [${BASH_SOURCE##*/}:${LINENO}] '
 
 set -x
 
-brew config
-brew doctor
-
-brew tap-info
-brew tap
-
-brew list --formula --versions
-brew list --formula --installed-on-request
-brew list --formula --no-installed-on-request
-brew list --pinned
-brew leaves
-brew leaves --installed-on-request
 brew list --cask --versions
 
-mas list --json
+mas list
 
-eza --long --all --group-directories-first --git-ignore /Applications/
+eza --long --group-directories-first /Applications/
+
+if [[ -d "$HOME/Applications" ]]; then
+  eza --long --group-directories-first "$HOME/Applications/"
+fi
